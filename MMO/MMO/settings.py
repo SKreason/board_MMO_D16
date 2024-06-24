95c48bac29558f9beb5e7a4d549a40078a6fa7ff
Sergey Kuryaev <skreason@ya.ru>
2024-06-25 00:09:29 +0500

    add static

--- MMO/MMO/settings.py
+++ MMO/MMO/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$)ku6=41$gp1ihq$tnupjy1#9q@4vr7u$+ajdhugc7rbc38h%#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+SITE_ID = 1
 
 # Application definition
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MMO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# адрес папки со статикой
+STATICFILES_DIRS = [BASE_DIR / 'static']
